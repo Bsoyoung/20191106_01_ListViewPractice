977b56eeac8531084973fcea4384b8bed1708c69
so_young <qksthdhd94@gmail.com>
2019-11-06 20:48:10 +0900

    리스트뷰 / 어댑터 연결 getView에서 데이터 출력

--- app/src/main/java/com/example/a20191106_01_listviewpractice/adapters/NoticeAdapter.kt
+++ app/src/main/java/com/example/a20191106_01_listviewpractice/adapters/NoticeAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.a20191106_01_listviewpractice.R
 import com.example.a20191106_01_listviewpractice.datas.NoticeData
 import kotlinx.android.synthetic.main.notice_list_item.view.*
@@ -27,6 +28,16 @@ class NoticeAdapter(context:Context,res:Int,list:ArrayList<NoticeData>) : ArrayA
         }
 //      tempRow가 null인 경우를 대비했으니, row에 tempRow는 절대 null이 아니라고 하면서 대입.
         var row = tempRow!!
+
+        var data = mList.get(position)
+
+        var titleTxt = row.findViewById<TextView>(R.id.noticeTitle)
+        var dateTxt = row.findViewById<TextView>(R.id.noticeDate)
+        var contentTxt = row.findViewById<TextView>(R.id.noticeContent)
+
+        titleTxt.text = data.noticeTitle
+        dateTxt.text = data.date
+        contentTxt.text = data.noticeContent
 
         return row
     }
